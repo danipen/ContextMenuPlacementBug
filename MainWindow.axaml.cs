d69bb5b9e2f171610d5f942d426a77c7cac146b0
Daniel <dpenalba@codicesoftware.com>
2021-12-14 13:52:54 +0100

    Reproduced the maximize issue!

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -11,11 +11,16 @@ namespace ContextMenuPlacementBug
     {
         public MainWindow()
         {
+            this.Activated += MainWindow_Activated;
+
             InitializeComponent();
 #if DEBUG
             this.AttachDevTools();
 #endif
 
+            this.Padding = new Thickness(0);
+            this.Margin = new Thickness(0);
+            
             List<MenuItem> menuItems = new List<MenuItem>();
             menuItems.Add(new MenuItem() { Header = "This is a long menu item 1" });
             menuItems.Add(new MenuItem() { Header = "This is a long menu item 2" });
@@ -25,9 +30,14 @@ namespace ContextMenuPlacementBug
             mContextMenu = new ContextMenu();
             mContextMenu.Items = menuItems;
 
-            mButton = this.FindControl<Button>("mButton");
-            //mButton.ContextMenu = mContextMenu;
-            mButton.Click += button_Click;
+            mButton = this.FindControl<Panel>("mButton");
+            mButton.Margin = new Thickness(0);
+            mButton.PointerPressed += button_Click;
+        }
+
+        private void MainWindow_Activated(object? sender, EventArgs e)
+        {
+            Console.WriteLine("WindowBounds: {0},{1}, {2}x{3} (IsMaximized={4})", this.Bounds.X, this.Bounds.Y, this.Bounds.Width, this.Bounds.Height, this.WindowState == WindowState.Maximized);
         }
 
         void button_Click(object? sender, RoutedEventArgs e)
@@ -43,6 +53,6 @@ namespace ContextMenuPlacementBug
         }
 
         ContextMenu mContextMenu;
-        Button mButton;
+        Panel mButton;
     }
 }
